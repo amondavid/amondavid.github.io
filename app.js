582f512f912054c355ea5b0b262f0ec8709174cd
amondavid <amonbaerwald@gmail.com>
2026-03-08 08:16:14 +0100

    create 'public' directory, create 3 route, 1 dynamic route

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 import express from 'express'
+import { logger } from './middlewares/logger.js'
 
 const app = express()
 const PORT = 5000 /* changed port to distinquish it from the 3000 one used in the tutorial */
 
-app.use(express.static('.'))
+app.use(logger)
+
+app.use(express.static('public')) 
 
 app.get('/', (request, response) => {
-  response.sendFile('index.html', { root: '.' })
-}) /* this operation makes it so that the server hosts my html pages instead of showing the message from the tutorial */
+  response.sendFile('index.html', { root: 'public' }) /*sendFile allows me to route directly to an html file*/
+})
+
+app.get('/about', (request, response) => {
+  response.sendFile('about.html', { root: 'public' })
+})
+
+app.get('/projects/:slug', (request, response) => {
+  const projectId = request.params.slug
+
+  response.sendFile(`${projectId}.html`,{ root: 'public'} )
+})
 
 app.listen(PORT, () => {
   console.log(`👋 Started server on port ${PORT}`)
